4f14189752d1beba57b381d288e672a565699c38
Tony Wu <bobabeach2@gmail.com>
2021-07-03 18:55:27 -0700

    Added button for sketch grid and made working hover effects

--- eas.js
+++ eas.js
@@ -1,19 +1,7 @@
 const gridContainer = document.querySelector('.container');
 const divButton = document.querySelector('.createDiv');
 
-function check(e) {
-    e.stopPropagation();
-    this.classList.add('paint');
-}
-
-
-// Attaching event listeners after the creation of divs.
-function hover() {
-    const b = document.querySelectorAll('.box');
-    b.forEach(box => box.addEventListener('mouseover', check, ));
-}
-
-divButton.addEventListener('click', divBoxes, {once: true}); // Button to create 16x16 grid of divs only works once
+divButton.addEventListener('click', divBoxes, {once: true}); // Button to create 16x16 grid of divs only works only once
 
 function divBoxes() {
     for(i = 1; i <= 16*16; i++) {
@@ -26,33 +14,15 @@ function divBoxes() {
 }
 
 
+function colorMe(e) {
+    e.stopPropagation();
+    this.classList.add('paint');
+}
 
-// function makeDiv() {
-//     const div = document.createElement('div');
-//     for (i = 0; i < 16*16; i++) {
-//         gridContainer.div;
-//     }
-// }
 
-// ^ Testing function to create a 16x16 grid
+// Attaching event listeners after the creation of divs.
+function hover() {
+    const b = document.querySelectorAll('.box');
+    b.forEach(box => box.addEventListener('mouseover', colorMe));
+}
 
-// Create multiple divs with similar classes
-// Experimented with creating boxes, except the first one to have a different class.
-
-// function divBoxes() {
-//         for(i = 1; i <= 16*16; i++) {
-//             if(i == 1) {
-//                 const div = document.createElement('div');
-//                 div.textContent = `${i}`;
-//                 div.classList.add(`box1`);
-//                 gridContainer.appendChild(div);
-//             } else if (i !== 1) {
-//                 const div = document.createElement('div');
-//                 div.textContent = `${i}`;
-//                 div.classList.add(`box`);
-//                 gridContainer.appendChild(div);
-//             } else {
-//                 return;
-//             }
-//         }
-//     }
