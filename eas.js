bde43d3537ae111649437e78fc5a64e1f940459c
Tony Wu <bobabeach2@gmail.com>
2021-07-11 21:50:42 -0700

    Finished Ethc-A-Sketch - Will come back later to add extras.

--- eas.js
+++ eas.js
@@ -1,17 +1,92 @@
 const gridContainer = document.querySelector('.container');
-const divButton = document.querySelector('.createDiv');
+const slider = document.querySelector('.slider');
+const sliderContainer = document.querySelector('.centerSlider');
+const newGrid = document.querySelector('.clear');
+const pSliderValue = document.querySelector('.sliderValue');
+const p = document.createElement('p');
+let range = 0;
 
-divButton.addEventListener('click', divBoxes, {once: true}); // Button to create 16x16 grid of divs only works only once
+// Creates paragraphs to hold live Grid and Slider values
+function createGridValue() {
+    if(sliderContainer.p == undefined){
+        sliderContainer.insertBefore(p, slider);
+        p.classList.add('size');
+    } else {
+        return;
+    }
+}
 
-function divBoxes() {
-    for(i = 1; i <= 16*16; i++) {
+
+// Clear button; Clears grid and prompts for a new N x N grid to be created
+newGrid.addEventListener('click', createGrid);
+
+function createGrid() {
+
+    createGridValue();
+    resetGrid();
+    const pGridValue = document.querySelector('.size');
+
+    let newRange = prompt("Please enter a number equal to or under 50 for new grid: ");
+    range = newRange;
+    if(range <= 50) {
+        pSliderValue.textContent = `${range}`;
+        pGridValue.textContent = `${range} x ${range}`;
+        gridContainer.style.gridTemplateColumns = `repeat(${range}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${range}, 1fr)`;
+        divBoxes(range);
+    } else {
+        return alert("That number is too high! Try again.");
+    }
+
+}
+
+// Separate function that just resets the grid
+function resetGrid() {
+    const divs = document.querySelectorAll('.squares');
+    divs.forEach(divs => divs.parentNode.removeChild(divs));
+}
+
+// Creates div squares with the range input for a total of range*range divs
+function divBoxes(x) {
+    for(i = 1; i <= x*x; i++) {
         const div = document.createElement('div');
-        div.classList.add('box');
+        div.classList.add('squares');
         gridContainer.appendChild(div);
     }
     // Calls a function to add eventlisteners after the creation of divs / squares
     hover();
 }
+
+
+// Live updates the value of the slider and displays it
+slider.addEventListener('mousemove', updateValue);
+pSliderValue.textContent = slider.value;
+
+function updateValue() {
+    pSliderValue.textContent = `${slider.value}`;
+}
+
+
+// Updates text which represents slider value
+// Also pre-sets the grid to desired dimensions
+
+slider.addEventListener('change', rangeValue);
+
+function rangeValue() {
+
+    createGridValue();
+    resetGrid();
+    const pGridValue = document.querySelector('.size');
+
+    pGridValue.textContent = `${slider.value} x ${slider.value}`;
+    range = slider.value;
+    gridContainer.style.gridTemplateColumns = `repeat(${range}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${range}, 1fr)`;
+    divBoxes(range);
+}
+
+// Add event listener for Slider to display value which updates
+// a paragraph created above it as the input changes
 
 
 function colorMe(e) {
@@ -22,7 +97,7 @@ function colorMe(e) {
 
 // Attaching event listeners after the creation of divs.
 function hover() {
-    const b = document.querySelectorAll('.box');
-    b.forEach(box => box.addEventListener('mouseover', colorMe));
+    const divs = document.querySelectorAll('.squares');
+    divs.forEach(squares => squares.addEventListener('mouseover', colorMe));
 }
 
